035f5a0c6ae69ea4df8518820d8d9b360b813a10
Thais Silveira <thais_lsilveira@hotmail.com>
2019-09-19 13:30:53 -0300

    Criação da classe DApresentacao

---
+++ CamadaDados/DApresentacao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace CamadaDados
+{
+    class DApresentacao
+    {
+        private int _Idapresentacao;
+        private string _Nome;
+        private string _Descricao;
+        private string _TextoBuscar;
+
+        public int Idapresentacao { get => _Idapresentacao; set => _Idapresentacao = value; }
+        public string Nome { get => _Nome; set => _Nome = value; }
+        public string Descricao { get => _Descricao; set => _Descricao = value; }
+        public string TextoBuscar { get => _TextoBuscar; set => _TextoBuscar = value; }
+    }
+}
